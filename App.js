19d3d3a8e089201d31162eb77d2ca7abde010789
abhaygedam <abhaygedam1999@gmail.com>
2021-09-30 13:16:27 +0530

    all src file is present

---
+++ App.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import { Routes } from "./Components/Routes/Routes";
+import { Button_getPrimePerMonth } from "./Components/Styled/ButtonTillCheckout";
+import LandingMain from "./Components/LandingMain";
+import TvShows from "./Components/TvShows";
+
+function App() {
+    return (
+      <>
+            {/* <LandingMain></LandingMain> */}
+           <TvShows></TvShows>
+            {/* <Routes /> */}
+        </>
+    );
+}
+
+export default App;
